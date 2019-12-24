91baa21d3f6b0074f84b1719cf578fc033bd57b7
qizhenpeng <qizhenpeng@wejoydata.com>
2019-12-24 08:56:06 +0800

    init

---
+++ product-service/src/main/java/com/example/product/web/ProductController.java
@@ -0,0 +1,11 @@
+package com.example.product.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("product")
+public class ProductController {
+
+
+}
