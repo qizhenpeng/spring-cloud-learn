c218cf57ea6b8ff3cbf6e83a0ff34650506083d0
qizhenpeng <qizhenpeng@wejoydata.com>
2020-01-07 00:00:52 +0800

    zuul

--- user-service/src/main/java/com/example/user/web/UserController.java
+++ user-service/src/main/java/com/example/user/web/UserController.java
@@ -2,6 +2,7 @@ package com.example.user.web;
 
 import com.example.user.entity.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,13 @@ public class UserController {
     @RequestMapping("get")
     public User getUser(@RequestParam("id") int id, HttpServletRequest request){
         log.info("service port:{},get user for id:{}",request.getServerPort(),id);
-        return new User(id,"user");
+        int i = RandomUtils.nextInt(10);
+        try {
+            Thread.sleep(i*1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new User(id,"user-"+request.getServerPort());
     }
 
 }
